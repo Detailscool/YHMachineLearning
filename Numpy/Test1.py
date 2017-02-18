d4808dc5764ba84955403229edfb98dc48326d04
liyuanhui <liyuanhui@qianduan.com>
2017-02-18 19:27:19 +0800

    commit numpy test

---
+++ Numpy/Test1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+if __name__ == "__main__":
+    # arr = np.arange(15).reshape(3, 5)
+    # print arr
+    # b = arr.T
+    # arr[1,2] = 5
+    # print b
+    # print arr
+
+    arr = np.random.rand(6,3)
+    print arr
